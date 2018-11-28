29cd0803591967f48996f84e3054d7f72ce47f31
Catarina <catariiniinh@hotmail.com>
2018-11-28 10:21:29 +0000

    Portfolio_Catarina

---
+++ Site_Portfolio_REV/index.js
@@ -0,0 +1,11 @@
+function showVideo(videoId) {
+    switch (videoId) {
+        case 1:
+            const dialogVideo = document.getElementById("varsovie")
+            dialogVideo.showModal()
+            break;
+
+        default:
+            break;
+    }
+}
