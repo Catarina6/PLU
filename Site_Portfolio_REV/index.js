8d44ae5278077fffa155ddf6f86a43456507b8d7
Catarina <catariiniinh@hotmail.com>
2018-12-01 20:17:10 +0000

    Portfolio

--- Site_Portfolio_REV/index.js
+++ Site_Portfolio_REV/index.js
@@ -1,11 +1,62 @@
 function showVideo(videoId) {
     switch (videoId) {
         case 1:
-            const dialogVideo = document.getElementById("varsovie")
+            const dialogVideo = document.getElementById("terror empire")
             dialogVideo.showModal()
             break;
 
         default:
             break;
     }
+    switch (videoId) {
+        case 2:
+            const dialogVideo = document.getElementById("varsovie")
+            dialogVideo.showModal()
+            break;
+        default:
+            break;
+    }
+    switch (videoId) {
+        case 3:
+            const dialogVideo = document.getElementById("aeon sable")
+            dialogVideo.showModal()
+            break;
+        default:
+            break;
+    }
+    switch (videoId) {
+        case 4:
+            const dialogVideo = document.getElementById("the godiva")
+            dialogVideo.showModal()
+            break;
+        default:
+            break;
+    }
+    switch (videoId) {
+        case 5:
+            const dialogVideo = document.getElementById("manes")
+            dialogVideo.showModal()
+            break;
+        default:
+            break;
+    }
+    switch (videoId) {
+        case 6:
+            const dialogVideo = document.getElementById("enblood")
+            dialogVideo.showModal()
+            break;
+        default:
+            break;
+    }
+
+    switch (videoId) {
+        case 7:
+            const dialogVideo = document.getElementById("varsovie1")
+            dialogVideo.showModal()
+            break;
+        default:
+            break;
+    }
+
 }
+
